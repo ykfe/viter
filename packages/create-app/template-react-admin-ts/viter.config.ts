87e223f7d0c9745ff4d137fbe67ca27df8e8ab34
jerryYuX <1342985759@qq.com>
2021-07-27 10:40:20 +0800

    feat:优化模板

--- packages/create-app/template-react-admin-ts/viter.config.ts
+++ packages/create-app/template-react-admin-ts/viter.config.ts
@@ -1,23 +1,22 @@
-import { defineConfig } from "viter";
-import reactRefresh from "@vitejs/plugin-react-refresh";
-const { resolve } = require("path");
+import { defineConfig } from 'viter';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
   routes: [
     {
-      path: "/",
-      component: "@/layout",
+      path: '/',
+      component: '@/layout',
       routes: [
-        { path: "/home", component: "./Home" },
-        { path: "/about", component: "./About" },
-        { path: "/404", component: "./NotFind" },
-        { path: "/*", redirect: "/404" },
+        { path: '/home', component: './Home' },
+        { path: '/about', component: './About' },
+        { path: '/404', component: './NotFind' },
+        { path: '/*', redirect: '/404' },
       ],
     },
   ],
   dynamicImport: {
-    loading: "@/loading",
+    loading: '@/loading',
   },
 });
